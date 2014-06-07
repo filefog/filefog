93b88eea9e504aa3ba50a80394e73e1e0c742243
jason <jk17@ualberta.ca>
2014-06-07 16:42:50 -0700

    removed unneded deps, bumped mocha version. Changed Filefog structure. Created empty initialize function for later.Created client and provider wrapper. Updated base tests. Created mocha opts file.

---
+++ test/wrapper/provider_wrapper.spec.js
@@ -0,0 +1,46 @@
+var should = require('should');
+var FileFog = require('../../lib/main.js');
+describe("ProviderWrapper",function(){
+    var provider;
+
+    before(function(){
+        var base_classes = {
+            provider : function(){},
+            transform: {},
+            client: function(){}
+        }
+        FileFog.use("empty",base_classes);
+        provider = FileFog.provider("empty");
+    });
+
+    it("should have a getConfig() method", function(){
+        provider.getConfig.should.be.a.Function;
+    })
+    it("should have a setConfig() method", function(){
+        provider.setConfig.should.be.a.Function;
+    })
+
+    it("should have an name property", function(){
+        provider.name.should.be.a.String;
+    })
+
+    describe("#getConfig()", function(){
+        it("should retrieve the value in ", function(){
+            provider.setConfig({test:"test"})
+            provider.getConfig().test.should.eql("test")
+        })
+    })
+
+    describe("#setConfig()", function(){
+        it("should extend existing values", function(){
+            provider.setConfig({test:"test", test2:"test2"})
+            provider.getConfig().test.should.eql("test");
+            provider.setConfig({test:"new_test"})
+            provider.getConfig().test.should.eql("new_test");
+
+        })
+    })
+
+
+
+})
